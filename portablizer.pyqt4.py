668092bfcebe9cdd0694aa4372620373ceea47dc
caiguanhao <caiguanhao@gmail.com>
2014-01-06 16:16:12 +0800

    run subprocess on start; kill any previously launched;

--- portablizer.pyqt4.py
+++ portablizer.pyqt4.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import psutil
+import subprocess
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -10,9 +12,14 @@ class Portablizer(QMainWindow):
   def __init__(self):
     QMainWindow.__init__(self);
     self.resize(1024, 768)
+    g = self.frameGeometry()
+    g.moveCenter(QDesktopWidget().availableGeometry().center())
+    self.move(g.topLeft())
     self.browser = QWebView()
     self.setCentralWidget(self.browser)
-    self.browser.setHtml('<strong>Hello World!</strong>')
+
+  def startBrowser(self):
+    self.browser.setHtml('<strong>Loading...</strong>')
     self.browser.show()
 
   def goto(self, url):
@@ -23,17 +30,55 @@ if getattr(sys, 'frozen', False):
 else:
   basedir = os.path.dirname(__file__)
 
+def nodedir(file):
+  return os.path.join(basedir, "node", file)
+
+nodeurl = 'http://127.0.0.1:3000/'
+
 app = QApplication([])
 app.setApplicationName('Portablizer')
 
 portablizer = Portablizer()
 portablizer.setWindowTitle('Portablizer')
+
+try:
+  startupinfo = subprocess.STARTUPINFO()
+  startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+except:
+  startupinfo = None
+
+for proc in psutil.process_iter():
+  try:
+    if proc.name == "node.exe" and proc.getcwd() == basedir:
+      proc.kill()
+  except:
+    pass
+
+try:
+  nodeprocess = subprocess.Popen([nodedir("node.exe"), nodedir("app.js")], startupinfo=startupinfo, stdout=subprocess.PIPE)
+except:
+  QMessageBox.warning(portablizer, "Error", "The application is missing required files. You need to re-install it.")
+  sys.exit(1)
+
 portablizer.show()
+portablizer.startBrowser()
+
+for line in iter(nodeprocess.stdout.readline, ''):
+  if "3000" in line:
+    portablizer.goto(nodeurl)
+    break
 
 menubar = portablizer.menuBar()
-helpMenu = menubar.addMenu('&Help')
-openweb = QAction('Open website', app)
-openweb.triggered.connect(lambda: portablizer.goto('http://en.m.wikipedia.org/wiki/Main_Page'))
+helpMenu = menubar.addMenu('&Options')
+openweb = QAction('Reload', app)
+openweb.triggered.connect(lambda: portablizer.goto(nodeurl))
 helpMenu.addAction(openweb)
 
-sys.exit(app.exec_())
+appprocess = app.exec_()
+
+try:
+  nodeprocess.kill()
+except:
+  pass
+
+sys.exit(appprocess)
